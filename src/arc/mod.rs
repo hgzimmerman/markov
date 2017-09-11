4056b4477fd84a2eff5c8f68657af6de580d46f1
Ziggypop <zimhen7@gmail.com>
2017-09-11 06:27:05 -0400

    added Arc support

--- src/arc/mod.rs
+++ src/arc/mod.rs
@@ -1,27 +1,3 @@
-//! A generic [Markov chain](https://en.wikipedia.org/wiki/Markov_chain) for almost any type. This
-//! uses HashMaps internally, and so Eq and Hash are both required.
-//!
-//! # Examples
-//!
-//! ```
-//! use markov::Chain;
-//!
-//! let mut chain = Chain::new();
-//! chain.feed_str("I like cats and I like dogs.");
-//! println!("{}", chain.generate_str());
-//! ```
-//!
-//! ```
-//! use markov::Chain;
-//!
-//! let mut chain = Chain::new();
-//! chain.feed(vec![1u8, 2, 3, 5]).feed(vec![3u8, 9, 2]);
-//! println!("{:?}", chain.generate());
-//! ```
-#![warn(missing_docs)]
-
-extern crate rand;
-
 use std::borrow::ToOwned;
 use std::collections::HashMap;
 use std::collections::hash_map::Entry::{Occupied, Vacant};
@@ -31,30 +7,30 @@ use std::io::BufReader;
 use std::io::prelude::*;
 use std::iter::Map;
 use std::path::Path;
-use std::rc::Rc;
+use std::sync::Arc;
 use rand::{Rng, thread_rng};
 
-pub mod arc;
 
 
 /// The definition of all types that can be used in a Chain.
 pub trait Chainable: Eq + Hash {}
 impl<T> Chainable for T where T: Eq + Hash {}
 
-type Token<T> = Option<Rc<T>>;
+type ArcToken<T> = Option<Arc<T>>;
 
 /// A generic [Markov chain](https://en.wikipedia.org/wiki/Markov_chain) for almost any type. This
 /// uses HashMaps internally, and so Eq and Hash are both required.
+/// The Arc version use atomic reference counting instead of Rc, to support sharing the chain across threads.
 #[derive(PartialEq, Debug)]
-pub struct Chain<T> where T: Chainable {
-    map: HashMap<Vec<Token<T>>, HashMap<Token<T>, usize>>,
+pub struct ArcChain<T> where T: Chainable {
+    map: HashMap<Vec<ArcToken<T>>, HashMap<ArcToken<T>, usize>>,
     order: usize,
 }
 
-impl<T> Chain<T> where T: Chainable {
+impl<T> ArcChain<T> where T: Chainable {
     /// Constructs a new Markov chain.
-    pub fn new() -> Chain<T> {
-        Chain {
+    pub fn new() -> ArcChain<T> {
+        ArcChain {
             map: {
                 let mut map = HashMap::new();
                 map.insert(vec!(None; 1), HashMap::new());
@@ -66,7 +42,7 @@ impl<T> Chain<T> where T: Chainable {
 
     /// Choose a specific Markov chain order. The order is the number of previous tokens to use
     /// as the index into the map.
-    pub fn order(&mut self, order: usize) -> &mut Chain<T> {
+    pub fn order(&mut self, order: usize) -> &mut ArcChain<T> {
         assert!(order > 0);
         self.order = order;
         self.map.insert(vec!(None; self.order), HashMap::new());
@@ -82,11 +58,11 @@ impl<T> Chain<T> where T: Chainable {
 
     /// Feeds the chain a collection of tokens. This operation is O(n) where n is the number of
     /// tokens to be fed into the chain.
-    pub fn feed(&mut self, tokens: Vec<T>) -> &mut Chain<T> {
+    pub fn feed(&mut self, tokens: Vec<T>) -> &mut ArcChain<T> {
         if tokens.is_empty() { return self }
         let mut toks = vec!(None; self.order);
         toks.extend(tokens.into_iter().map(|token| {
-            Some(Rc::new(token))
+            Some(Arc::new(token))
         }));
         toks.push(None);
         for p in toks.windows(self.order + 1) {
@@ -99,7 +75,7 @@ impl<T> Chain<T> where T: Chainable {
     /// Generates a collection of tokens from the chain. This operation is O(mn) where m is the
     /// length of the generated collection, and n is the number of possible states from a given
     /// state.
-    pub fn generate(&self) -> Vec<Rc<T>> {
+    pub fn generate(&self) -> Vec<Arc<T>> {
         let mut ret = Vec::new();
         let mut curs = vec!(None; self.order);
         loop {
@@ -116,8 +92,8 @@ impl<T> Chain<T> where T: Chainable {
     /// operation is O(mn) where m is the length of the generated collection, and n is the number
     /// of possible states from a given state. This returns an empty vector if the token is not
     /// found.
-    pub fn generate_from_token(&self, token: T) -> Vec<Rc<T>> {
-        let token = Rc::new(token);
+    pub fn generate_from_token(&self, token: T) -> Vec<Arc<T>> {
+        let token = Arc::new(token);
         if !self.map.contains_key(&vec!(Some(token.clone()); self.order)) { return Vec::new() }
         let mut ret = vec![token.clone()];
         let mut curs = vec!(Some(token.clone()); self.order);
@@ -142,29 +118,29 @@ impl<T> Chain<T> where T: Chainable {
     }
 }
 
-impl Chain<String> {
+impl ArcChain<String> {
     /// Feeds a string of text into the chain.
-    pub fn feed_str(&mut self, string: &str) -> &mut Chain<String> {
+    pub fn feed_str(&mut self, string: &str) -> &mut ArcChain<String> {
         self.feed(string.split(' ').map(|s| s.to_owned()).collect())
     }
 
     /// Feeds a properly formatted file into the chain. This file should be formatted such that
     /// each line is a new sentence. Punctuation may be included if it is desired.
-    pub fn feed_file<P: AsRef<Path>>(&mut self, path: P) -> &mut Chain<String> {
+    pub fn feed_file<P: AsRef<Path>>(&mut self, path: P) -> &mut ArcChain<String> {
         let reader = BufReader::new(File::open(path).unwrap());
         for line in reader.lines() {
             let line = line.unwrap();
             let words = line.split_whitespace()
-                            .filter(|word| !word.is_empty())
-                            .map(|s| s.to_owned())
-                            .collect();
+                .filter(|word| !word.is_empty())
+                .map(|s| s.to_owned())
+                .collect();
             self.feed(words);
         }
         self
     }
 
     /// Converts the output of generate(...) on a String chain to a single String.
-    fn vec_to_string(vec: Vec<Rc<String>>) -> String {
+    fn vec_to_string(vec: Vec<Arc<String>>) -> String {
         let mut ret = String::new();
         for s in &vec {
             ret.push_str(&s);
@@ -179,41 +155,41 @@ impl Chain<String> {
 
     /// Generates a random string of text.
     pub fn generate_str(&self) -> String {
-        Chain::vec_to_string(self.generate())
+        ArcChain::vec_to_string(self.generate())
     }
 
     /// Generates a random string of text starting with the desired token. This returns an empty
     /// string if the token is not found.
     pub fn generate_str_from_token(&self, string: &str) -> String {
-        Chain::vec_to_string(self.generate_from_token(string.to_owned()))
+        ArcChain::vec_to_string(self.generate_from_token(string.to_owned()))
     }
 
     /// Produces an infinite iterator of generated strings.
     pub fn str_iter(&self) -> InfiniteChainStringIterator {
-        let vec_to_string: fn(Vec<Rc<String>>) -> String = Chain::vec_to_string;
+        let vec_to_string: fn(Vec<Arc<String>>) -> String = ArcChain::vec_to_string;
         self.iter().map(vec_to_string)
     }
 
     /// Produces a sized iterator of generated strings.
     pub fn str_iter_for(&self, size: usize) -> SizedChainStringIterator {
-        let vec_to_string: fn(Vec<Rc<String>>) -> String = Chain::vec_to_string;
+        let vec_to_string: fn(Vec<Arc<String>>) -> String = ArcChain::vec_to_string;
         self.iter_for(size).map(vec_to_string)
     }
 }
 
 /// A sized iterator over a Markov chain of strings.
 pub type SizedChainStringIterator<'a> =
-Map<SizedChainIterator<'a, String>, fn(Vec<Rc<String>>) -> String>;
+Map<SizedChainIterator<'a, String>, fn(Vec<Arc<String>>) -> String>;
 
 /// A sized iterator over a Markov chain.
 pub struct SizedChainIterator<'a, T: Chainable + 'a> {
-    chain: &'a Chain<T>,
+    chain: &'a ArcChain<T>,
     size: usize,
 }
 
 impl<'a, T> Iterator for SizedChainIterator<'a, T> where T: Chainable + 'a {
-    type Item = Vec<Rc<T>>;
-    fn next(&mut self) -> Option<Vec<Rc<T>>> {
+    type Item = Vec<Arc<T>>;
+    fn next(&mut self) -> Option<Vec<Arc<T>>> {
         if self.size > 0 {
             self.size -= 1;
             Some(self.chain.generate())
@@ -230,16 +206,16 @@ impl<'a, T> Iterator for SizedChainIterator<'a, T> where T: Chainable + 'a {
 
 /// An infinite iterator over a Markov chain of strings.
 pub type InfiniteChainStringIterator<'a> =
-Map<InfiniteChainIterator<'a, String>, fn(Vec<Rc<String>>) -> String>;
+Map<InfiniteChainIterator<'a, String>, fn(Vec<Arc<String>>) -> String>;
 
 /// An infinite iterator over a Markov chain.
 pub struct InfiniteChainIterator<'a, T: Chainable + 'a> {
-    chain: &'a Chain<T>
+    chain: &'a ArcChain<T>
 }
 
 impl<'a, T> Iterator for InfiniteChainIterator<'a, T> where T: Chainable + 'a {
-    type Item = Vec<Rc<T>>;
-    fn next(&mut self) -> Option<Vec<Rc<T>>> {
+    type Item = Vec<Arc<T>>;
+    fn next(&mut self) -> Option<Vec<Arc<T>>> {
         Some(self.chain.generate())
     }
 }
@@ -247,20 +223,20 @@ impl<'a, T> Iterator for InfiniteChainIterator<'a, T> where T: Chainable + 'a {
 /// A collection of states for the Markov chain.
 trait States<T: PartialEq> {
     /// Adds a state to this states collection.
-    fn add(&mut self, token: Token<T>);
+    fn add(&mut self, token: ArcToken<T>);
     /// Gets the next state from this collection of states.
-    fn next(&self) -> Token<T>;
+    fn next(&self) -> ArcToken<T>;
 }
 
-impl<T> States<T> for HashMap<Token<T>, usize> where T: Chainable {
-    fn add(&mut self, token: Token<T>) {
+impl<T> States<T> for HashMap<ArcToken<T>, usize> where T: Chainable {
+    fn add(&mut self, token: ArcToken<T>) {
         match self.entry(token) {
             Occupied(mut e) => *e.get_mut() += 1,
             Vacant(e) => { e.insert(1); },
         }
     }
 
-    fn next(&self) -> Token<T> {
+    fn next(&self) -> ArcToken<T> {
         let mut sum = 0;
         for &value in self.values() {
             sum += value;
@@ -275,118 +251,5 @@ impl<T> States<T> for HashMap<Token<T>, usize> where T: Chainable {
             }
         }
         unreachable!("The random number generator failed.")
-    }
-}
-
-#[cfg(test)]
-mod test {
-    use super::Chain;
-
-    #[test]
-    fn new() {
-        Chain::<u8>::new();
-        Chain::<String>::new();
-    }
-
-    #[test]
-    fn is_empty() {
-        let mut chain = Chain::new();
-        assert!(chain.is_empty());
-        chain.feed(vec![1u8, 2, 3]);
-        assert!(!chain.is_empty());
-    }
-
-    #[test]
-    fn feed() {
-        let mut chain = Chain::new();
-        chain.feed(vec![3, 5, 10]).feed(vec![5, 12]);
-    }
-
-    #[test]
-    fn generate() {
-        let mut chain = Chain::new();
-        chain.feed(vec![3u8, 5, 10]).feed(vec![5, 12]);
-        let v = chain.generate().into_iter().map(|v| *v).collect();
-        assert!([vec![3, 5, 10], vec![3, 5, 12], vec![5, 10], vec![5, 12]].contains(&v));
-    }
-
-    #[test]
-    fn generate_for_higher_order() {
-        let mut chain = Chain::new();
-        chain.order(2);
-        chain.feed(vec![3u8, 5, 10]).feed(vec![2, 3, 5, 12]);
-        let v = chain.generate().into_iter().map(|v| *v).collect();
-        assert!([vec![3, 5, 10], vec![3, 5, 12], vec![2, 3, 5, 10], vec![2, 3, 5, 12]].contains(&v));
-    }
-
-    #[test]
-    fn generate_from_token() {
-        let mut chain = Chain::new();
-        chain.feed(vec![3u8, 5, 10]).feed(vec![5, 12]);
-        let v = chain.generate_from_token(5).into_iter().map(|v| *v).collect();
-        assert!([vec![5, 10], vec![5, 12]].contains(&v));
-    }
-
-    #[test]
-    fn generate_from_unfound_token() {
-        let mut chain = Chain::new();
-        chain.feed(vec![3u8, 5, 10]).feed(vec![5, 12]);
-        let v: Vec<_> = chain.generate_from_token(9).into_iter().map(|v| *v).collect();
-        assert_eq!(v, vec![]);
-    }
-
-    #[test]
-    fn iter() {
-        let mut chain = Chain::new();
-        chain.feed(vec![3u8, 5, 10]).feed(vec![5, 12]);
-        assert_eq!(chain.iter().size_hint().1, None);
-    }
-
-    #[test]
-    fn iter_for() {
-        let mut chain = Chain::new();
-        chain.feed(vec![3u8, 5, 10]).feed(vec![5, 12]);
-        assert_eq!(chain.iter_for(5).collect::<Vec<_>>().len(), 5);
-    }
-
-    #[test]
-    fn feed_str() {
-        let mut chain = Chain::new();
-        chain.feed_str("I like cats and dogs");
-    }
-
-    #[test]
-    fn generate_str() {
-        let mut chain = Chain::new();
-        chain.feed_str("I like cats").feed_str("I hate cats");
-        assert!(["I like cats", "I hate cats"].contains(&&chain.generate_str()[..]));
-    }
-
-    #[test]
-    fn generate_str_from_token() {
-        let mut chain = Chain::new();
-        chain.feed_str("I like cats").feed_str("cats are cute");
-        assert!(["cats", "cats are cute"].contains(&&chain.generate_str_from_token("cats")[..]));
-    }
-
-    #[test]
-    fn generate_str_from_unfound_token() {
-        let mut chain = Chain::new();
-        chain.feed_str("I like cats").feed_str("cats are cute");
-        assert_eq!(chain.generate_str_from_token("test"), "");
-    }
-
-    #[test]
-    fn str_iter() {
-        let mut chain = Chain::new();
-        chain.feed_str("I like cats and I like dogs");
-        assert_eq!(chain.str_iter().size_hint().1, None);
-    }
-
-    #[test]
-    fn str_iter_for() {
-        let mut chain = Chain::new();
-        chain.feed_str("I like cats and I like dogs");
-        assert_eq!(chain.str_iter_for(5).collect::<Vec<_>>().len(), 5);
     }
 }
